888518fb9f2953198c7f14060552f98b48fc9edb
Michael Buffington <michael.buffington@gmail.com>
2020-08-17 16:29:23 -0600

    Fixed the idiotic warning that you couldn't turn off no matter what, and that's supposedly been fixed in hugginface/transformers but has not been.

--- generate.py
+++ generate.py
@@ -1,126 +1,3 @@
-# Usage:
-#
-# This works in python 3. It might in python 2 as well, but I have no idea.
-#
-# There are a ton of tools that let you do rvm or rbenv like things with python,
-# but I have no idea what's best. I used pyenv.
-#
-# Installing dependencies:
-#
-# I think this is what bare minimum list, installed by doing `pip install [packagename]`
-# `pip install console`
-# `pip install transformers`
-# `pip install nltk`
-#
-# You may also need:
-#
-# `pip install tensorflow`
-#
-#
-#
-# Using it, assuming everything installs properly
-#
-#
-# python generate.py
-#
-# (wait longer than you'd like)
-#
-# Eventually you'll get a prompt.
-# Assume that everything surrounded in { } is what the program is outputting.
-# Anything else is my commentary.
-#
-# {
-#   Enter a command to do something. eg `view cycles`.
-#   >
-# }
-#
-#
-# If you just want some sweet robot decided words, type:
-# {
-#  > gen
-# }
-#
-# Short for generate. This will take the default "story" and feed it
-# to the GTP-2 model. It can take some time. Like, 5 to 10 seconds.
-#
-# Every time you run `gen`, it takes whatever the story is, then adds the robot
-# generated text to the story. The next time you run `gen`, it uses the initial
-# story plus whatever else the robot has generated.
-#
-# Which means as you run `gen`, the story builds, but it can also get a little
-# slower each time.
-#
-# To view the default story:
-#
-# {
-#   > view story
-#   "I enjoy walking with my cute dog"
-# }
-#
-# You can change the default story, but you should first do a reset if you've already
-# generated some text using `gen`.
-#
-# {
-#   > reset
-#   > set story Eggs litter the ground, despite the lack of birds.
-# }
-#
-# You can then run `gen`, using whatever you've set the story to be as your
-# starting point. The generator will happily finish sentences as well. So you
-# if you do `set story I am a silly`, it'll complete the sentence.
-#
-# At any point you can add anything to the current story.
-#
-# {
-#  > add Giovanni was unsurprised by Kyle's outfit.
-# }
-#
-# This just gets appeneded to the story so far without having the any robot
-# stuff happen. It can help with making things cohesive.
-# {
-#  > set story Eggs litter the ground, despite the lack of birds.
-#  story set to: Eggs litter the ground, despite the lack of birds.
-#  > gen
-#  Eggs litter the ground, despite the lack of birds. A flock of pigeons, led by the dim-witted but good-natured Mr. Boggs, try to catch a fly, but are unsuccessful.
-#  > add Mr. Boggs, having a small mind, persists.
-#  > view story
-#  story: Eggs litter the ground, despite the lack of birds. A flock of pigeons, led by the dim-witted but good-natured Mr. Boggs, try to catch a fly, but are unsuccessful. Mr. Boggs, having a small mind, persists.
-# }
-#
-# There are a few other settings you can tweak at any time:
-# {
-#   > view base_length
-#   base_length: 30
-#   > set base_length 40
-# }
-#
-# base_length controls how long of a sentence you want the robot to generate each time you run `gen`.
-# If it's really short (like 20 or under), it has a harder time writing stuff.
-# If it's really long, it can write really verbose and often fluent things, but it takes AGES to do.
-# 30 to 40 seems to work well.
-# {
-#   > view cycles
-#   cycles: 2
-#   > set cycles 3
-# }
-#
-# cycles controls how many times the robot is asked to generate stuff per `gen`.
-# I can't remember why this isn't just set to 1 by default.
-# Odds are, it's because the text would often end half finished. So, it'd do
-# "My dog is a monster. He eats a"
-# I think I didn't like that incomplete sentence, so I have it do two cycles to
-# increase the odds of at least one full sentence per `gen`.
-# I also throw away incomplete sentences. So with cycles set 1, I might end up
-# with no text generated most of the time.
-# It's worth remembering that if 1 cycle takes about 5 seconds to run, 2 takes 10.
-#
-#
-# Other commands:
-# reset - resets the story and all settings to defaults
-# exit - closes this thing.
-
-## CODE
-
 # Stuff that makes this an interactive console-ish thing
 import re
 from time import time
@@ -128,9 +5,7 @@ from cmd import Cmd
 from console import fg, bg, fx
 
 # All the language generation stuff
-from transformers import GPT2Tokenizer, GPT2LMHeadModel, GPT2Model
-from transformers import AutoTokenizer, AutoModelWithLMHead, AutoModelForCausalLM
-from transformers import TFGPT2Model
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 # This text into sentences. Fun fact, it's hard to know what the sentences are in
 # the following:
@@ -148,10 +23,6 @@ from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktTrainer
 # faster than the standard GPT-2 model as well.
 model = GPT2LMHeadModel.from_pretrained('pranavpsv/genre-story-generator-v2')
 tokenizer = GPT2Tokenizer.from_pretrained('pranavpsv/genre-story-generator-v2')
-
-# tokenizer = GPT2LMHeadModel.from_pretrained("sshleifer/tiny-gpt2")
-# model = GPT2Tokenizer.from_pretrained("sshleifer/tiny-gpt2")
-
 
  # Some models may have this string in their output. We want to scrub it out.
 stop_token = '<|endoftext|>'
@@ -232,7 +103,8 @@ def gen(seed, cycle):
         num_return_sequences=num_return_sequences,
         num_beams=num_beams,
         no_repeat_ngram_size=no_repeat_ngram_size,
-        early_stopping=early_stopping
+        early_stopping=early_stopping,
+        pad_token_id = 50256
     )
     # try:
     final = tokenizer.decode(output_sequences[0], skip_special_tokens=True)
@@ -329,7 +201,7 @@ class CmdMenu(Cmd):
             elif setting == "temperature":
                 new_value = float(value)
             elif setting == "top_k":
-                new_value = float(value)
+                new_value = int(value)
             elif setting == "top_p":
                 new_value = float(value)
             elif setting == "min_length":
